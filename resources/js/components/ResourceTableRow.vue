60892952662b70542ebff59f556ddee1954f90bb
Eric Eaglstun <eeaglstun@curiousmedia.com>
2021-02-23 13:28:14 -0700

    finish imcomplete merge

--- resources/js/components/ResourceTableRow.vue
+++ resources/js/components/ResourceTableRow.vue
@@ -113,75 +113,6 @@
 							viaRelationship: viaRelationship,
 						},
 					}"
-<<<<<<< HEAD
-          :title="__('Edit')"
-        >
-          <icon type="edit" />
-        </router-link>
-      </span>
-
-      <!-- Delete Resource Link -->
-      <button
-        :data-testid="`${testId}-delete-button`"
-        :dusk="`${resource['id'].value}-delete-button`"
-        class="appearance-none cursor-pointer text-70 hover:text-primary mr-3"
-        v-if="resource.authorizedToDelete && (!resource.softDeleted || viaManyToMany)"
-        @click.prevent="openDeleteModal"
-        :title="__(viaManyToMany ? 'Detach' : 'Delete')"
-      >
-        <icon />
-      </button>
-
-      <!-- Restore Resource Link -->
-      <button
-        :dusk="`${resource['id'].value}-restore-button`"
-        class="appearance-none cursor-pointer text-70 hover:text-primary mr-3"
-        v-if="resource.authorizedToRestore && resource.softDeleted && !viaManyToMany"
-        @click.prevent="openRestoreModal"
-        :title="__('Restore')"
-      >
-        <icon type="restore" with="20" height="21" />
-      </button>
-
-      <portal to="modals">
-        <transition name="fade">
-          <delete-resource-modal
-            v-if="deleteModalOpen"
-            @confirm="confirmDelete"
-            @close="closeDeleteModal"
-            :mode="viaManyToMany ? 'detach' : 'delete'"
-          >
-            <div slot-scope="{ uppercaseMode, mode }" class="p-8">
-              <heading :level="2" class="mb-6">
-                {{
-                __(uppercaseMode + ' Resource')
-                }}
-              </heading>
-              <p
-                class="text-80 leading-normal"
-              >{{ __('Are you sure you want to ' + mode + ' this resource?') }}</p>
-            </div>
-          </delete-resource-modal>
-        </transition>
-
-        <transition name="fade">
-          <restore-resource-modal
-            v-if="restoreModalOpen"
-            @confirm="confirmRestore"
-            @close="closeRestoreModal"
-          >
-            <div class="p-8">
-              <heading :level="2" class="mb-6">{{ __('Restore Resource') }}</heading>
-              <p
-                class="text-80 leading-normal"
-              >{{ __('Are you sure you want to restore this resource?') }}</p>
-            </div>
-          </restore-resource-modal>
-        </transition>
-      </portal>
-    </td>
-  </tr>
-=======
 					:title="__('Edit')"
 				>
 					<icon type="edit" />
@@ -246,72 +177,10 @@
 			</portal>
 		</td>
 	</tr>
->>>>>>> 6b8c6660854806de53bb79958965f7097fd14133
 </template>
 
 <script>
 export default {
-<<<<<<< HEAD
-  props: [
-    "disabledSort",
-    "testId",
-    "deleteResource",
-    "restoreResource",
-    "resource",
-    "resourcesSelected",
-    "resourceName",
-    "relationshipType",
-    "viaRelationship",
-    "viaResource",
-    "viaResourceId",
-    "viaManyToMany",
-    "checked",
-    "actionsAreAvailable",
-    "shouldShowCheckboxes",
-    "updateSelectionStatus"
-  ],
-
-  data: () => ({
-    deleteModalOpen: false,
-    restoreModalOpen: false
-  }),
-
-  methods: {
-    /**
-     * Select the resource in the parent component
-     */
-    toggleSelection() {
-      this.updateSelectionStatus(this.resource);
-    },
-
-    openDeleteModal() {
-      this.deleteModalOpen = true;
-    },
-
-    confirmDelete() {
-      this.deleteResource(this.resource);
-      this.closeDeleteModal();
-    },
-
-    closeDeleteModal() {
-      this.deleteModalOpen = false;
-    },
-
-    openRestoreModal() {
-      this.restoreModalOpen = true;
-    },
-
-    confirmRestore() {
-      this.restoreResource(this.resource);
-      this.closeRestoreModal();
-    },
-
-    closeRestoreModal() {
-      this.restoreModalOpen = false;
-    }
-  }
-};
-=======
 	props: [
 		'disabledSort',
 		'testId',
@@ -378,5 +247,4 @@ export default {
 		},
 	},
 }
->>>>>>> 6b8c6660854806de53bb79958965f7097fd14133
 </script>
