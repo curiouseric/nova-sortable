f0c096519a1c1d6e4bc7865b74a3b96cdb41a766
Eric Eaglstun <eeaglstun@curiousmedia.com>
2021-02-23 13:26:59 -0700

    testing

--- resources/js/components/ResourceTable.vue
+++ resources/js/components/ResourceTable.vue
@@ -68,170 +68,154 @@
 import { InteractsWithResourceInformation } from "laravel-nova";
 
 export default {
-  mixins: [InteractsWithResourceInformation],
+	mixins: [InteractsWithResourceInformation],
 
-  props: {
-    authorizedToRelate: {
-      type: Boolean,
-      required: true
-    },
-    resourceName: {
-      default: null
-    },
-    resources: {
-      default: []
-    },
-    singularName: {
-      type: String,
-      required: true
-    },
-    selectedResources: {
-      default: []
-    },
-    selectedResourceIds: {},
-    shouldShowCheckboxes: {
-      type: Boolean,
-      default: false
-    },
-    actionsAreAvailable: {
-      type: Boolean,
-      default: false
-    },
-    viaResource: {
-      default: null
-    },
-    viaResourceId: {
-      default: null
-    },
-    viaRelationship: {
-      default: null
-    },
-    relationshipType: {
-      default: null
-    },
-    updateSelectionStatus: {
-      type: Function
-    }
-  },
+	props: {
+		authorizedToRelate: {
+			type: Boolean,
+			required: true,
+		},
+		resourceName: {
+			default: null,
+		},
+		resources: {
+			default: [],
+		},
+		singularName: {
+			type: String,
+			required: true,
+		},
+		selectedResources: {
+			default: [],
+		},
+		selectedResourceIds: {},
+		shouldShowCheckboxes: {
+			type: Boolean,
+			default: false,
+		},
+		actionsAreAvailable: {
+			type: Boolean,
+			default: false,
+		},
+		viaResource: {
+			default: null,
+		},
+		viaResourceId: {
+			default: null,
+		},
+		viaRelationship: {
+			default: null,
+		},
+		relationshipType: {
+			default: null,
+		},
+		updateSelectionStatus: {
+			type: Function,
+		},
+	},
 
-  data() {
-    return {
-      selectAllResources: false,
-      selectAllMatching: false,
-      getResourcesForProps: this.resources,
-      resourceCount: null,
-      disabledSort: false
-    };
-  },
+	data() {
+		return {
+			selectAllResources: false,
+			selectAllMatching: false,
+			getResourcesForProps: this.resources,
+			resourceCount: null,
+			disabledSort: false,
+		}
+	},
 
-  watch: {
-    resources(newVal, oldValue) {
-      if (newVal !== oldValue) {
-        this.getResourcesForProps = this.resources;
-      }
-    }
-  },
+	watch: {
+		resources(newVal, oldValue) {
+			if (newVal !== oldValue) {
+				this.getResourcesForProps = this.resources
+			}
+		}
+	},
 
-  methods: {
-    /**
-     * Delete the given resource.
-     */
-    deleteResource(resource) {
-      this.$emit("delete", [resource]);
-    },
+	methods: {
+		/**
+		 * Delete the given resource.
+		 */
+		deleteResource(resource) {
+			this.$emit('delete', [resource])
+		},
 
-    /**
-     * Restore the given resource.
-     */
-    restoreResource(resource) {
-      this.$emit("restore", [resource]);
-    },
+		/**
+		 * Restore the given resource.
+		 */
+		restoreResource(resource) {
+			this.$emit('restore', [resource])
+		},
 
-    /**
-     * Broadcast that the ordering should be updated.
-     */
-    requestOrderByChange(field) {
-      this.$emit("order", field);
-    },
+		/**
+		 * Broadcast that the ordering should be updated.
+		 */
+		requestOrderByChange(field) {
+			this.$emit('order', field)
+		},
 
-    async updatedSortable(event) {
-      // yuckkkkk
-      let pagination = this.$parent.$parent.$parent.$parent;
+		async updatedSortable(event) {
+			this.disabledSort = true
+			let items = btoa(JSON.stringify(
+				this.getResourcesForProps.map((item, key) => {
+					return {
+						id: item.id.value,
+						sort_order: 1 + key,
+					}
+				})
+			))
 
-      //   console.log("updatedSortable", "event.newIndex", event.newIndex);
-      //   console.log("pagination.currentPage", pagination.currentPage);
-      //   console.log("pagination.perPage", pagination.perPage);
+			try {
+				const response = await Nova.request().post(
+					`/nova-vendor/nova-sortable/${this.resourceName}/sortable`,
+					{items}
+				)
 
-      // for debugging
-      window._event = event;
-      window._pagination = pagination;
+				this.$toasted.show(
+					this.__('The new order has been set!'),
+					{type: 'success'}
+				);
 
-      let data = {
-        id: event.item.__vue__.resource.sort_id,
-        index: event.newIndex,
-        page: pagination.currentPage,
-        sort_on: event.item.__vue__.resource.sort_on,
-        viaRelationship: this.viaRelationship,
-        viaResource: this.viaResource,
-        viaResourceId: this.viaResourceId
-      };
+				this.disabledSort = false
+				// this.$router.go(this.$router.currentRoute)
+			}
+			catch(e) {
+				this.$toasted.show(
+					this.__('An error occured while trying to reorder the resource.'),
+					{type: 'error'}
+				);
+			}
+			finally {
+				// statements
+			}
+		},
+	},
 
-      try {
-        const response = await Nova.request()
-          .post(
-            `/nova-vendor/nova-sortable/${this.resourceName}/sortable`,
-            data
-          )
-          .then(data => {
-            // update table with new sort order
-            this.resources = data.data.resources;
-          });
+	computed: {
+		/**
+		 * Get all of the available fields for the resources.
+		 */
+		fields() {
+			if (this.getResourcesForProps) {
+				return this.getResourcesForProps[0].fields
+			}
+		},
 
-        this.$toasted.show(this.__("The new order has been set!"), {
-          type: "success"
-        });
+		/**
+		 * Determine if the current resource listing is via a many-to-many relationship.
+		 */
+		viaManyToMany() {
+			return (
+				this.relationshipType == 'belongsToMany' || this.relationshipType == 'morphToMany'
+			)
+		},
 
-        this.disabledSort = false;
-        // this.$router.go(this.$router.currentRoute)
-      } catch (e) {
-        this.$toasted.show(
-          this.__("An error occured while trying to reorder the resource."),
-          { type: "error" }
-        );
-      } finally {
-        // statements
-      }
-    }
-  },
-
-  computed: {
-    /**
-     * Get all of the available fields for the resources.
-     */
-    fields() {
-      if (this.getResourcesForProps) {
-        return this.getResourcesForProps[0].fields;
-      }
-    },
-
-    /**
-     * Determine if the current resource listing is via a many-to-many relationship.
-     */
-    viaManyToMany() {
-      return (
-        this.relationshipType == "belongsToMany" ||
-        this.relationshipType == "morphToMany"
-      );
-    },
-
-    /**
-     * Determine if the current resource listing is via a has-one relationship.
-     */
-    viaHasOne() {
-      return (
-        this.relationshipType == "hasOne" || this.relationshipType == "morphOne"
-      );
-    }
-  }
-};
+		/**
+		 * Determine if the current resource listing is via a has-one relationship.
+		 */
+		viaHasOne() {
+			return this.relationshipType == 'hasOne' || this.relationshipType == 'morphOne'
+		},
+	},
+}
 </script>
